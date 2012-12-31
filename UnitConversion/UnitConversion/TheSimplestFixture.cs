aac6f638817e6e82c16af3470bb7ed3b4e82db5b
Anton Martynenko <anton.a.martynenko@gmail.com>
2012-12-31 04:34:04 +0200

    The Simplest case implemented - unit tested and refactored

--- UnitConversion/UnitConversion/TheSimplestFixture.cs
+++ UnitConversion/UnitConversion/TheSimplestFixture.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using NUnit.Framework;
 
 namespace UnitConversion
@@ -11,45 +12,37 @@ namespace UnitConversion
         [Test]
         public void TheSimplest1()
         {
-            var result = TheSimplestInput1().Solve(new List<Func<dynamic>>()).Where(x => x != null);
-            foreach (var s in result)
-            {
-                Console.WriteLine(s().ToString());
-            }
-            Console.ReadLine();
+            var result = TheSimplestInput1().CheckAndEvaluateEach(new List<Func<dynamic>>()).Format().ConcatAll();
+
+            Console.WriteLine(result);
+            Assert.That(result, Is.EqualTo("120 second = 2 minute\r\n3 minute = 180 second\r\n"));
         }
 
         [Test]
         public void TheSimplest2()
         {
-            var result = TheSimplestInput2().Solve(new List<Func<dynamic>>()).Where(x => x != null);
-            foreach (var s in result)
-            {
-                Console.WriteLine(s().ToString());
-            }
-            Console.ReadLine();
+            var result = TheSimplestInput2().CheckAndEvaluateEach(new List<Func<dynamic>>()).Format().ConcatAll();
+
+            Console.WriteLine(result);
+            Assert.That(result, Is.EqualTo("2 minute = 120 second\r\n3 minute = 180 second\r\n"));
         }
 
         [Test]
         public void TheSimplest3()
         {
-            var result = TheSimplestInput3().Solve(new List<Func<dynamic>>()).Where(x => x != null);
-            foreach (var s in result)
-            {
-                Console.WriteLine(s().ToString());
-            }
-            Console.ReadLine();
+            var result = TheSimplestInput3().CheckAndEvaluateEach(new List<Func<dynamic>>()).Format().ConcatAll();
+
+            Console.WriteLine(result);
+            Assert.That(result, Is.EqualTo("120 second = 2 minute\r\n180 second = 3 minute\r\n"));
         }
 
         [Test]
         public void TheSimplest4()
         {
-            var result = TheSimplestInput4().Solve(new List<Func<dynamic>>()).Where(x => x != null);
-            foreach (var s in result)
-            {
-                Console.WriteLine(s().ToString());
-            }
-            Console.ReadLine();
+            var result = TheSimplestInput4().CheckAndEvaluateEach(new List<Func<dynamic>>()).Format().ConcatAll();
+
+            Console.WriteLine(result);
+            Assert.That(result, Is.EqualTo("2 minute = 120 second\r\n180 second = 3 minute\r\n"));
         }
 
         #region the simplest input
@@ -117,52 +110,83 @@ namespace UnitConversion
 
     static class FunctionContainer
     {
-        public static IEnumerable<Func<dynamic>> Solve(this IEnumerable<Func<dynamic>> input, List<Func<dynamic>> accumulator)
+        public static string ConcatAll(this IEnumerable<string> strings)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var s in strings)
+                sb.Append(s);
+
+            return sb.ToString();
+        }
+
+        public static IEnumerable<string> Format(this IEnumerable<Func<dynamic>> facts)
+        {
+            foreach (var fact in facts)
+            {
+                yield return string.Format("{0} {1} = {2} {3}\r\n",
+                    fact().Left.Amount, fact().Left.UnitName,
+                    fact().Right.Amount, fact().Right.UnitName);
+            }
+        }
+
+        public static IEnumerable<Func<dynamic>> CheckAndEvaluateEach(this IEnumerable<Func<dynamic>> input, List<Func<dynamic>> evaluatedAlready)
         {
             foreach (var equation in input)
             {
                 if (equation().Right.Amount == null)
-                    accumulator = accumulator.Foo(equation, accumulator);
+                    evaluatedAlready.AddRange(equation.TryEvaluate(evaluatedAlready, new List<Func<dynamic>>()).Where(x => x() != null));
                 else
-                    accumulator.Add(equation);
+                    evaluatedAlready.Add(equation);
             }
 
-            foreach (var result in accumulator)
-                yield return result;
+            foreach (var evaluatedEquation in evaluatedAlready)
+                yield return evaluatedEquation;
         }
 
-        public static List<Func<dynamic>> Foo(this IEnumerable<Func<dynamic>> list, Func<dynamic> equation, List<Func<dynamic>> accumulator)
+        public static Func<dynamic> Evaluate(this Func<dynamic> unknown, Func<dynamic> fact)
         {
-            Func<Func<dynamic>, Func<dynamic>, Func<dynamic>> findRelation = (unknown, item) =>
-            {
-                if (unknown().Left.UnitName == item().Left.UnitName)
-                {
-                    return () => new
-                    {
-                        unknown().Left,
-                        Right = new { Amount = unknown().Left.Amount * (item().Right.Amount / item().Left.Amount), unknown().Right.UnitName }
-                    };
-                }
-                if (unknown().Left.UnitName == item().Right.UnitName)
-                {
-                    return () => new
-                    {
-                        unknown().Left,
-                        Right = new { Amount = unknown().Left.Amount * (item().Left.Amount / item().Right.Amount), unknown().Right.UnitName }
-                    };
-                }
-                return () => null;
-            };
+            if (unknown.LeftUnitMatchesFactLeftUnit(fact))
+                return () => unknown.LeftEvaluate(fact);
 
-            foreach (var resolvedEquation in list)
+            if (unknown.LeftUnitMatchesFactRightUnit(fact))
+                return () => unknown.RightEvaluate(fact);
+
+            return () => null;
+        }
+
+        private static object RightEvaluate(this Func<dynamic> unknown, Func<dynamic> fact)
+        {
+            return new
             {
-                var newRelation = findRelation(equation, resolvedEquation);
-                if (newRelation != null)
-                {
-                    accumulator.Add(newRelation);
-                    break;
-                }
-            }
+                unknown().Left,
+                Right = new { Amount = unknown().Left.Amount * (fact().Left.Amount / fact().Right.Amount), unknown().Right.UnitName }
+            };
+        }
+
+        private static object LeftEvaluate(this Func<dynamic> unknown, Func<dynamic> fact)
+        {
+            return new
+            {
+                unknown().Left,
+                Right = new { Amount = unknown().Left.Amount * (fact().Right.Amount / fact().Left.Amount), unknown().Right.UnitName }
+            };
+        }
+
+        private static dynamic LeftUnitMatchesFactLeftUnit(this Func<dynamic> unknown, Func<dynamic> fact)
+        {
+            return unknown().Left.UnitName == fact().Left.UnitName;
+        }
+
+        private static dynamic LeftUnitMatchesFactRightUnit(this Func<dynamic> unknown, Func<dynamic> fact)
+        {
+            return unknown().Left.UnitName == fact().Right.UnitName;
+        }
+
+        public static List<Func<dynamic>> TryEvaluate(this Func<dynamic> unknown, IEnumerable<Func<dynamic>> alreadyEvaluated, List<Func<dynamic>> accumulator)
+        {
+            foreach (var fact in alreadyEvaluated)
+                accumulator.Add(unknown.Evaluate(fact));
 
             return accumulator;
         }
