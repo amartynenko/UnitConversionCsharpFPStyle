6b52df194d32eaf00689e31330b53f2a30de98d8
Anton Martynenko <anton.a.martynenko@gmail.com>
2012-12-31 02:51:29 +0200

    Some refactorings...

--- UnitConversion/UnitConversion/Another.cs
+++ UnitConversion/UnitConversion/Another.cs
@@ -5,134 +5,13 @@ using NUnit.Framework;
 
 namespace UnitConversion
 {
-    public interface Input : IEnumerable<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>> { }
-
-    public class Another
-    {
-        //private readonly double value;
-        //private readonly string unitName;
-
-        //private readonly Func<Unit> parent;
-
-        //private readonly List<Func<Unit>> relatedUnits = new List<Func<Unit>>();
-
-        //public Unit(Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>> equation, Func<Unit> parent)
-        //{
-        //    this.parent = parent;
-        //    relatedUnits.Add(() => new Unit(equation().Item1.Item1.Value, equation.Item1.Item2, parent));
-
-        //    var unit = Solve(equation);
-        //    value = equation.Item2.Item1.Value;
-        //    unitName = equation.Item2.Item2;
-
-        //    var parentUnit = parent();
-        //    if (parentUnit != null)
-        //    {
-        //        relatedUnits.AddRange(parentUnit.RelatedUnits());
-        //    }
-        //}
-
-        //public Unit(Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>> equation)
-        //    : this(equation, null)
-        //{
-        //}
-
-        //public Unit(Unit unit, Func<Unit> parent)
-        //{
-        //    this.parent = parent;
-        //    relatedUnits.AddRange(unit.RelatedUnits());
-        //    relatedUnits.Add(parent);
-        //}
-
-        //public Unit(double value, string unitName, Func<Unit> parent)
-        //{
-        //    this.value = value;
-        //    this.unitName = unitName;
-        //    this.parent = parent;
-        //}
-
-        //private IEnumerable<Func<Unit>> RelatedUnits()
-        //{
-        //    return relatedUnits;
-        //}
-
-        //public Unit Solve(Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>> equation)
-        //{
-        //    if (equation().Item2.Item1 != null)
-        //        return new Unit();
-
-        //    foreach (var relatedUnit in relatedUnits)
-        //    {
-        //        if (relatedUnit().unitName == equation().Item1.Item2)
-        //        {
-        //            return new Unit(relatedUnit().value, relatedUnit().unitName, () => this);
-        //        }
-        //    }
-
-        //    return null;
-        //}
-
-        //public override string ToString()
-        //{
-        //    return parent().value + " " + parent().unitName + " = " + value + " " + unitName;
-        //}
-    }
-
     [TestFixture]
-    public class Foo
+    public class TheSimplestFixture
     {
-        //[Test]
-        //public void Run()
-        //{
-        //    Func<Func<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>, Tuple<IEnumerable<Unit>, List<string>>, Tuple<IEnumerable<Unit>, List<string>>>, IEnumerable<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>>, Tuple<IEnumerable<Unit>, List<string>>, IEnumerable<string>> a = (foreachEquation, x, accumulator) =>
-        //    {
-        //        foreach (var equation in x)
-        //        {
-        //            accumulator = foreachEquation(equation, accumulator);
-        //        }
-        //        return null;
-        //    };
-
-        //    Func<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>, Tuple<IEnumerable<Unit>, List<string>>, Tuple<IEnumerable<Unit>, List<string>>> c = (equation, accumulator) =>
-        //    {
-        //        var tree = new List<Unit>();
-        //        var list = new List<string>(accumulator.Item2.Select(result => result));
-
-        //        foreach (var unit in accumulator.Item1.Select(unit => unit))
-        //        {
-        //            var result = unit.Solve(equation);
-        //            if (result != null)
-        //            {
-        //                tree.Add(result);
-        //                if (equation.Item2 == null)
-        //                    accumulator.Item2.Add(result.ToString());
-
-        //                return new Tuple<IEnumerable<Unit>, List<string>>(tree, list);
-        //            }
-
-        //            accumulator.Item2.Add("Can not be resolved");
-        //        }
-
-        //        tree.Add(new Unit(equation, () => null));
-
-        //        return new Tuple<IEnumerable<Unit>, List<string>>(tree, list);
-        //    };
-
-        //    var whatever = a(c, TheSimplest(), new Tuple<IEnumerable<Unit>, List<string>>(new List<Unit>(), new List<string>()));
-        //    Console.WriteLine();
-        //}
-
-        ////private List<string> Calculate<Equation, List<string>>()
-        ////{
-
-        ////}
-
-
-
         [Test]
         public void TheSimplest1()
         {
-            var result = TheSimplestInput1().Print(new List<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>>()).Where(x => x != null);
+            var result = TheSimplestInput1().Print(new List<Func<dynamic>>()).Where(x => x != null);
             foreach (var s in result)
             {
                 Console.WriteLine(s().ToString());
@@ -143,7 +22,7 @@ namespace UnitConversion
         [Test]
         public void TheSimplest2()
         {
-            var result = TheSimplestInput2().Print(new List<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>>()).Where(x => x != null);
+            var result = TheSimplestInput2().Print(new List<Func<dynamic>>()).Where(x => x != null);
             foreach (var s in result)
             {
                 Console.WriteLine(s().ToString());
@@ -154,7 +33,7 @@ namespace UnitConversion
         [Test]
         public void TheSimplest3()
         {
-            var result = TheSimplestInput3().Print(new List<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>>()).Where(x => x != null);
+            var result = TheSimplestInput3().Print(new List<Func<dynamic>>()).Where(x => x != null);
             foreach (var s in result)
             {
                 Console.WriteLine(s().ToString());
@@ -165,7 +44,7 @@ namespace UnitConversion
         [Test]
         public void TheSimplest4()
         {
-            var result = TheSimplestInput4().Print(new List<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>>()).Where(x => x != null);
+            var result = TheSimplestInput4().Print(new List<Func<dynamic>>()).Where(x => x != null);
             foreach (var s in result)
             {
                 Console.WriteLine(s().ToString());
@@ -173,105 +52,110 @@ namespace UnitConversion
             Console.ReadLine();
         }
 
-        private IEnumerable<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>> TheSimplestInput1()
+        #region the simplest input
+        private IEnumerable<Func<dynamic>> TheSimplestInput1()
         {
-            yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                new Tuple<double?, string>(120.0, "second"),
-                new Tuple<double?, string>(2, "minute"));
-            
-            yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                new Tuple<double?, string>(1, "minute"),
-                new Tuple<double?, string>(null, "second"));
+            yield return () => new
+            {
+                Item1 = new Tuple<double?, string>(120.0, "second"),
+                Item2 = new Tuple<double?, string>(2, "minute")
+            };
+
+            yield return () => new
+            {
+                Item1 = new Tuple<double?, string>(1, "minute"),
+                Item2 = new Tuple<double?, string>(null, "second")
+            };
         }
 
-        private IEnumerable<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>> TheSimplestInput2()
+        private IEnumerable<Func<dynamic>> TheSimplestInput2()
         {
-            yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                new Tuple<double?, string>(2, "minute"),
-                new Tuple<double?, string>(120.0, "second"));
-            
-            yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                new Tuple<double?, string>(1, "minute"),
-                new Tuple<double?, string>(null, "second"));
+            yield return () => new
+            {
+                Item1 = new Tuple<double?, string>(2, "minute"),
+                Item2 = new Tuple<double?, string>(120.0, "second")
+            };
+
+            yield return () => new
+            {
+                Item1 = new Tuple<double?, string>(1, "minute"),
+                Item2 = new Tuple<double?, string>(null, "second")
+            };
         }
 
-        private IEnumerable<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>> TheSimplestInput3()
+        private IEnumerable<Func<dynamic>> TheSimplestInput3()
         {
-            yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                new Tuple<double?, string>(120.0, "second"),
-                new Tuple<double?, string>(2, "minute"));
+            yield return () => new
+            {
+                Item1 = new Tuple<double?, string>(120.0, "second"),
+                Item2 = new Tuple<double?, string>(2, "minute")
+            };
 
-            yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                new Tuple<double?, string>(60, "second"),
-                new Tuple<double?, string>(null, "minute"));
+            yield return () => new
+            {
+                Item1 = new Tuple<double?, string>(60, "second"),
+                Item2 = new Tuple<double?, string>(null, "minute")
+            };
         }
 
-        private IEnumerable<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>> TheSimplestInput4()
+        private IEnumerable<Func<dynamic>> TheSimplestInput4()
         {
-            yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                new Tuple<double?, string>(2, "minute"),
-                new Tuple<double?, string>(120.0, "second"));
+            yield return () => new
+            {
+                Item1 = new Tuple<double?, string>(2, "minute"),
+                Item2 = new Tuple<double?, string>(120.0, "second")
+            };
 
-            yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                new Tuple<double?, string>(60, "second"),
-                new Tuple<double?, string>(null, "minute"));
+            yield return () => new
+            {
+                Item1 = new Tuple<double?, string>(60, "second"),
+                Item2 = new Tuple<double?, string>(null, "minute")
+            };
         }
     }
+        #endregion
 
     static class SomeExtensions
     {
-        public static IEnumerable<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>> Print(this IEnumerable<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>> input, List<Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>> accumulator)
+        public static IEnumerable<Func<dynamic>> Print(this IEnumerable<Func<dynamic>> input, List<Func<dynamic>> accumulator)
         {
             foreach (var equation in input)
             {
                 if (equation().Item2.Item1 == null)
                 {
-                    Func<
-                        Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>,
-                        Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>,
-                        Func<Tuple<Tuple<double?, string>, Tuple<double?, string>>>>
-                    findRelation = (unknown, item) =>
+                    Func<Func<dynamic>, Func<dynamic>, Func<dynamic>> findRelation = (unknown, item) =>
                         {
-                            ////120.0 second = 2 minute
-                            //yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                            //    new Tuple<double?, string>(2, "minute"),
-                            //    new Tuple<double?, string>(120.0, "second"));
-                            ////1 minute = ? second
-                            //yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                            //    new Tuple<double?, string>(1, "minute"),
-                            //    new Tuple<double?, string>(null, "second"));
                             if (unknown().Item1.Item2 == item().Item1.Item2)
                             {
-                                return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                                    new Tuple<double?, string>(unknown().Item1.Item1, unknown().Item1.Item2),
-                                    new Tuple<double?, string>(unknown().Item1.Item1 * (item().Item2.Item1 / item().Item1.Item1), unknown().Item2.Item2));
+                                return () => new
+                                {
+                                    Item1 = new Tuple<double?, string>(unknown().Item1.Item1, unknown().Item1.Item2),
+                                    Item2 = new Tuple<double?, string>(unknown().Item1.Item1 * (item().Item2.Item1 / item().Item1.Item1), unknown().Item2.Item2)
+                                };
                             }
                             if (unknown().Item2.Item2 == item().Item2.Item2)
                             {
-                                return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                                    new Tuple<double?, string>(unknown().Item1.Item1, unknown().Item1.Item2),
-                                    new Tuple<double?, string>(unknown().Item1.Item1 / (item().Item1.Item1 / item().Item2.Item1), unknown().Item2.Item2));
+                                return () => new
+                                {
+                                    Item1 = new Tuple<double?, string>(unknown().Item1.Item1, unknown().Item1.Item2),
+                                    Item2 = new Tuple<double?, string>(unknown().Item1.Item1 / (item().Item1.Item1 / item().Item2.Item1), unknown().Item2.Item2)
+                                };
                             }
-                            //
-                            ////120.0 second = 2 minute
-                            //yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                            //    new Tuple<double?, string>(120.0, "second"),
-                            //    new Tuple<double?, string>(2, "minute"));
-                            ////1 minute = ? second
-                            //yield return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                            //    new Tuple<double?, string>(1, "minute"),
-                            //    new Tuple<double?, string>(null, "second"));
                             if (unknown().Item1.Item2 == item().Item2.Item2)
                             {
-                                return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                                    new Tuple<double?, string>(unknown().Item1.Item1, unknown().Item1.Item2),
-                                    new Tuple<double?, string>(unknown().Item1.Item1 * (item().Item1.Item1 / item().Item2.Item1), unknown().Item2.Item2));
+                                return () => new
+                                {
+                                    Item1 = new Tuple<double?, string>(unknown().Item1.Item1, unknown().Item1.Item2),
+                                    Item2 = new Tuple<double?, string>(unknown().Item1.Item1 * (item().Item1.Item1 / item().Item2.Item1), unknown().Item2.Item2)
+                                };
                             }
                             if (unknown().Item2.Item2 == item().Item1.Item2)
                             {
-                                return () => new Tuple<Tuple<double?, string>, Tuple<double?, string>>(
-                                    new Tuple<double?, string>(5.6, unknown().Item1.Item2),
-                                    new Tuple<double?, string>(null, item().Item2.Item2));
+                                return () => new
+                                {
+                                    Item1 = new Tuple<double?, string>(5.6, unknown().Item1.Item2),
+                                    Item2 = new Tuple<double?, string>(null, item().Item2.Item2)
+                                };
                             }
                             return () => null;
                         };
@@ -297,12 +181,5 @@ namespace UnitConversion
                 yield return result;
             }
         }
-    }
-
-
-
-    internal class Bar
-    {
-
     }
 }
